f7e310e8cdba2a37c560bfb3f1bf1efd586d16db
Thalen <tabadia7574@student.pps.net>
2022-08-24 16:21:35 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Twitter Account Sentiment Analysis
+
+Twitter Account Sentiment Analysis uses nltk Sentiment Analysis to see the sentiment in a users past 500 tweets.
+
+## Installation
+
+Make sure python is installed
+
+Run the command:
+
+```bash
+pip install -r requirements.txt
+```
+Open main.py
+
+## Thank you!!
---
+++ main.py
@@ -0,0 +1,59 @@
+# IMPORTS
+import nltk
+from colorama import Fore, Style
+from nltk.sentiment import SentimentIntensityAnalyzer
+import tweepy
+from dotenv import load_dotenv
+import os
+
+def configure():
+    load_dotenv()
+
+nltk.download('vader_lexicon')
+
+# INTRODUCTION
+print(Style.BRIGHT)
+print(Fore.BLUE)
+print("Welcome to the Twitter Account Sentiment Analyzer!")
+
+# TWITTER API AUTHENTICATION
+print(Fore.BLUE)
+username=input('Enter a username: ')
+print(Fore.WHITE)
+print("Accessing Twitter API... (This may take a while)")
+auth = tweepy.OAuthHandler(os.getenv('key1'), os.getenv('key2'))
+auth.set_access_token(os.getenv('key3'), os.getenv('key4'))
+api = tweepy.API(auth, wait_on_rate_limit=True)
+tweets_list = []
+for tweet in tweepy.Cursor(api.user_timeline, screen_name=username, count=None, tweet_mode='extended', exclude_replies=True, include_rts=False).items(500):
+  tweets_list.append(tweet)
+
+# SENTIMENT ANALYSIS
+print(Fore.BLUE)
+print("Calculating...")
+print(Fore.WHITE)
+print(f"Tweets found: {len(tweets_list)}")
+sia = SentimentIntensityAnalyzer()
+pos = 0
+neg = 0
+neu = 0
+total = 0
+
+for tweet in tweets_list:
+  total += 1
+  result = sia.polarity_scores(tweet.full_text)
+  if result['pos'] > result['neg']:
+    pos += 1
+  elif result['neg'] > result['pos']:
+    neg += 1
+  elif result['neu'] > 0.2:
+    neu += 1
+  else:
+    total -= 1
+
+# OUTPUT
+neu = neu/total
+pos = pos/total
+neg = neg/total
+print(Fore.BLUE)
+print(f"Positive: {round(pos*100,2)}% \nNegative: {round(neg*100,2)}% \nNeutral: {round(neu*100,2)}%")
